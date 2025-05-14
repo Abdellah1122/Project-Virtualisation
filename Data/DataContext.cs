746ae7e71714a7ea074c3b548f15b691d04dff03
umaimaes <essaoudoumaima@gmail.com>
2025-05-14 23:30:01 +0200

    added docker conf and kubernetes

---
+++ Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectVirtuqlisqtion.Models;
+
+namespace ProjectVirtuqlisqtion.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<CourseSection> CourseSections { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<WhatYouWillLearn> Whats { get; set; }
+        public DbSet<QuizOption> QuizOptions { get; set; }
+        public DbSet<QuizQuestion> QuizQuestions { get; set; }
+    }
+}
